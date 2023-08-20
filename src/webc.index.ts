3221103f74e523c41c790ac8acac1920ee1d37ad
Jonas Dutzi <jonas.dutzi@sap.com>
2023-08-20 21:13:10 +0200

    export all webcomponents

--- src/webc.index.ts
+++ src/webc.index.ts
@@ -1,6 +1,13 @@
-//export * from "../src/lib/components/index";
 export default () => {
     import("./lib/components/Sankey.svelte");
     import("./lib/components/SankeyChart.svelte");
+    import("./lib/components/Anchor.svelte");
+    import("./lib/components/Column.svelte");
+    import("./lib/components/ColumnContent.svelte");
+    import("./lib/components/ColumnHeader.svelte");
+    import("./lib/components/Group.svelte");
+    import("./lib/components/Item.svelte");
     import("./lib/components/Label.svelte");
+    import("./lib/components/Link.svelte");
+    import("./lib/components/SankeyLine.svelte");
 };
