2f7dc7d5618c95772cd813ed16ba5db20e12e952
Jonas Dutzi <jonas.dutzi@sap.com>
2023-09-08 15:08:24 +0200

    style test app

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import { sankeyData } from "./testdata/data.bigger.ts";
     import { sankeyData as sankeyData2 } from "./testdata/data.ts";
-    import { ColumnContent, ColumnHeader, Item, Link, Sankey } from "./lib/index";
     import SankeyInspector from "./inspector/Inspector.svelte";
     import SankeyChart from "./lib/components/SankeyChart.svelte";
 
     const onItemClicked = (e) => {
-        console.log("clicked item: ", e.detail.item);
+        console.log(e.detail.item);
     };
 
     let size = 30;
@@ -16,10 +15,10 @@
 <main>
     <h1 style="margin-block: 2rem;">Svelte Sankey</h1>
     <h2 style="margin-bottom: 1rem;">Settings/Props:</h2>
-    <div style="display: flex; align-items: center; gap: 1rem">
+    <div style="display: flex; align-items: center; gap: 2rem">
         <div>
             <div>Size (maxpathheight):</div>
-            <input type="range" min="1" max="100" bind:value={size} />
+            <input type="range" min="30" max="200" bind:value={size} />
         </div>
         <div>
             <div>Headers (showheaders):</div>
@@ -27,9 +26,11 @@
         </div>
         <div style="margin-bottom: 2rem;" />
     </div>
-    <div>__________________________________________________</div>
+    <div style="margin-bottom: 3rem;">__________________________________________________</div>
     <!-- <SankeyChart chartdata={sankeyData} showheaders={true} maxpathheight={40} /> -->
-    <SankeyChart showheaders={showHeaders} maxpathheight={size} chartdata={sankeyData} on:itemclick={onItemClicked} />
+    <!-- <SankeyChart showheaders={showHeaders} maxpathheight={size} chartdata={sankeyData} on:itemclick={onItemClicked} /> -->
+    <SankeyChart showheaders={showHeaders} maxpathheight={size} chartdata={sankeyData2} on:itemclick={onItemClicked} />
+    <div style="margin-top: 3rem;">__________________________________________________</div>
     <SankeyInspector />
 </main>
 
