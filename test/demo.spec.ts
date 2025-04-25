ccd0282ca3ac11dd18f22bd7c4c988abdcf3959c
Jonas Dutzi <43247491+JonasDutzi@users.noreply.github.com>
2025-04-25 14:47:14 +0200

    add test (#20)
    * add test
    
    * add coverage
    
    ---------
    
    Co-authored-by: d070292 <jonas.dutzi@sap.com>

--- test/demo.spec.ts
+++ test/demo.spec.ts
@@ -1,7 +1,10 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
+describe("sum test", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+  it("adds 2 + 3 to equal 5", () => {
+    expect(2 + 3).toBe(5);
+  });
 });
