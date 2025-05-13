2f251672c58025543de2e3aff61c1e74e43120bb
d070292 <jonas.dutzi@sap.com>
2025-05-13 10:53:06 +0200

    add coverage really!

--- test/unit/logging.spec.ts
+++ test/unit/logging.spec.ts
@@ -2,7 +2,7 @@ import { describe, it, expect, vi } from "vitest";
 import { logWarning } from "../../src/lib/helper";
 
 describe("log", () => {
-  it("should return 0 if the value is 0", () => {
+  it("should log a warning with a prefix", () => {
     const consoleMock = vi
       .spyOn(console, "warn")
       .mockImplementation(() => undefined);
