f754947c9fb4b3a1c0aa8cfaa35764f834d1f759
Jonas Dutzi <43247491+JonasDutzi@users.noreply.github.com>
2025-05-13 11:06:05 +0200

    test log (#32)
    * test log
    
    * add coverage really!
    
    * change
    
    * add a test
    
    ---------
    
    Co-authored-by: d070292 <jonas.dutzi@sap.com>

---
+++ test/unit/logging.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, vi } from "vitest";
+import { logError, logWarning } from "../../src/lib/helper";
+
+describe("log", () => {
+  it("should log a warning with a prefix", () => {
+    const consoleMock = vi
+      .spyOn(console, "warn")
+      .mockImplementation(() => undefined);
+    logWarning("a warning");
+    expect(consoleMock).toHaveBeenCalledWith("svelte-sankey: ", "a warning");
+  });
+  it("should log an error with a prefix", () => {
+    const consoleMock = vi
+      .spyOn(console, "error")
+      .mockImplementation(() => undefined);
+    logError("an error");
+    expect(consoleMock).toHaveBeenCalledWith("svelte-sankey: ", "an error");
+  });
+});
