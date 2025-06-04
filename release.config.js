70dc50152a2d48ff44d2eb1bc931e3f497ba3135
d070292 <jonas.dutzi@sap.com>
2025-06-04 08:50:30 +0200

    add plugins

--- release.config.js
+++ release.config.js
@@ -1,3 +1,7 @@
 export default {
   branches: ["main", { name: "next", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+  ],
 };
