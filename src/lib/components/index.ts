9b198f1d7b5668d986e80cb9ff6cc4beec7650f5
d070292 <jonas.dutzi@sap.com>
2025-06-04 08:44:23 +0200

    fix: remove unused group component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,11 +1,21 @@
 import Sankey from "./Sankey.svelte";
 import SankeyChart from "./SankeyChart.svelte";
 import Anchor from "./Anchor.svelte";
+import AnchorContent from "./AnchorContent.svelte";
 import Column from "./Column.svelte";
 import ColumnContent from "./ColumnContent.svelte";
 import ColumnHeader from "./ColumnHeader.svelte";
-import Group from "./Group.svelte";
 import Item from "./Item.svelte";
 import Label from "./Label.svelte";
 
-export { Sankey, SankeyChart, Anchor, Column, ColumnContent, ColumnHeader, Group, Item, Label };
+export {
+  Sankey,
+  SankeyChart,
+  Anchor,
+  AnchorContent,
+  Column,
+  ColumnContent,
+  ColumnHeader,
+  Item,
+  Label,
+};
