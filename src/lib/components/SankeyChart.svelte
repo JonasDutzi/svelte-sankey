889a76541a94e038e9af96e3e3363470ac7d70bf
d070292 <jonas.dutzi@sap.com>
2025-07-18 22:46:05 +0200

    fix: event handlers

--- src/lib/components/SankeyChart.svelte
+++ src/lib/components/SankeyChart.svelte
@@ -36,8 +36,8 @@
 		chartdata
 	}: Props = $props();
 
-	const onItemClicked = (item: any) => {
-		dispatchCustomEvent($host(), onItemClick, "itemclick", item);
+	const forwardEvent = (handler: ((data: any) => void) | undefined, eventName: string, data: any) => {
+		dispatchCustomEvent($host(), handler, eventName, data);
 	};
 </script>
 
@@ -57,7 +57,13 @@
 					<Item {item}>
 						<div class="anchor-group" style:--anchor-position={columnIndex === chartdata.data.length - 1 ? "row-reverse" : "row"}>
 							<Anchor {item} {onAnchorClick} />
-							<AnchorContent onItemClick={() => onItemClicked(item)} {onAnchorMouseEnter} {onAnchorMouseLeave} {item} {highlightpaths}>
+							<AnchorContent
+								onItemClick={() => forwardEvent(onItemClick, "itemclick", item)}
+								onAnchorMouseEnter={() => forwardEvent(onAnchorMouseEnter, "anchormouseenter", item)}
+								onAnchorMouseLeave={() => forwardEvent(onAnchorMouseLeave, "anchormouseleave", item)}
+								{item}
+								{highlightpaths}
+							>
 								<div>{item.label}</div>
 							</AnchorContent>
 						</div>
