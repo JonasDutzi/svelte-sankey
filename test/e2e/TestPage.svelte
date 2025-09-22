a1a957a6df77830fd9e6a059175b7580c7b68420
d070292 <jonas.dutzi@sap.com>
2025-09-22 08:40:07 +0200

    chore: automated accessibility testing

--- test/e2e/TestPage.svelte
+++ test/e2e/TestPage.svelte
@@ -1,5 +1,53 @@
-<script>
-	import App from "../../src/App.svelte";
+<script lang="ts">
+	import "./index.css";
+	import { sankeyData as data } from "../../src/testdata/data.customdata.ts";
+
+	import SankeyChart from "../../src/lib/components/SankeyChart.svelte";
+	import type { OnItemClick } from "../../src/lib/index.ts";
+
+	const onItemClick: OnItemClick = (item) => {
+		messages.push(JSON.stringify(item));
+	};
+
+	//basic example of resizing based on window breakpoints
+	$effect(() => {
+		window.addEventListener("resize", () => {
+			console.log("resized");
+			if (window.innerWidth > 1025) {
+				return; //use default size
+			} else if (window.innerWidth > 768) {
+				size = 150;
+			} else {
+				size = 60;
+			}
+		});
+	});
+
+	let messages = $state<Array<string>>([]);
+
+	let highlightPaths = $state(true);
+	let showHeaders = $state(true);
+	let size = $state(50);
 </script>
 
-<App></App>
+<main>
+	<SankeyChart highlightpaths={highlightPaths} showheaders={showHeaders} maxpathheight={size} minpathheight={1} chartdata={data} {onItemClick} />
+	<div>
+		<ul>
+			{#each messages as message}
+				<li>{message}</li>{/each}
+		</ul>
+	</div>
+</main>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: sans-serif;
+	}
+	:global(body) {
+		background-color: hsl(0, 0%, 93%);
+	}
+</style>
--- test/e2e/sankeyDiagram.spec.ts
+++ test/e2e/sankeyDiagram.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from "./base";
 
-test("render sankey diagram", async ({ page }) => {
+test("render sankey diagram", async ({ page, axe }) => {
 	await page.goto("http://localhost:5173/test/e2e/tests.html");
-	await expect(page.getByRole("button", { name: "Flour" })).toBeVisible();
-	await expect(page.getByRole("button", { name: "Biscuit" })).toBeVisible();
-	await expect(page.getByRole("button", { name: "Chocolate Production" })).toBeVisible();
-	await expect(page.getByRole("button", { name: "Semi-finished good" })).toBeVisible();
-	await expect(page.getByRole("button", { name: "Strawberry Cake" })).toBeVisible();
-	const cakeLabel = await page.getByRole("button", { name: "Chocolate Cake" });
+	const axeResults = await axe().analyze();
+	expect(axeResults.violations).toEqual([]);
+	await expect(page.getByRole("button", { name: "Flour", exact: true })).toBeVisible();
+	await expect(page.getByRole("button", { name: "Biscuit", exact: true })).toBeVisible();
+	await expect(page.getByRole("button", { name: "Chocolate Production", exact: true })).toBeVisible();
+	await expect(page.getByRole("button", { name: "Semi-finished good", exact: true })).toBeVisible();
+	await expect(page.getByRole("button", { name: "Strawberry Cake", exact: true })).toBeVisible();
+	const cakeLabel = await page.getByRole("button", { name: "Chocolate Cake", exact: true });
 	await expect(cakeLabel).toBeVisible();
 	await cakeLabel.click();
 	await expect(page.getByRole("listitem")).toContainText('{"id":"chococake","label":"Chocolate Cake","data":{"totalValue":8},"anchorColor":"var(--anchor-color-6)"}');
